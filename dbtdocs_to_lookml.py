8fcc33ede964938a4f71268445952e6f1069bfac
Matt Strautmann <matt.strautmann@airbnb.com>
2020-01-28 15:03:36 -0800

    init branch

--- dbtdocs_to_lookml.py
+++ dbtdocs_to_lookml.py
@@ -1,24 +1,73 @@
 import lkml
-import yaml
+import yaml ### install the pyyaml package
 
 import os
 import json
 
-# Hardcoded paths that should be parameteized later
-PATH_TO_DBT_PROJECT = "test_dbt_project"
-PATH_TO_LOOKML_PROJECT = "test_lookml_project"
-PATH_TO_TARGET_LOOKML_PROJECT = "target"
+import time
+import git  # Install `pip install GitPython`
+from git import RemoteProgress
 
-MANIFEST_FILE = "target/manifest.json"
+import csv
+from lookerapi import LookerApi
+
+# Hardcoded paths that should be parameteized later
+PATH_TO_DBT_PROJECT = "PATH_TO_DBT_PROJECT"
+GIT_URL = "https://github.com"
+BRANCH =  "looker_dev_branch_name"
+REPO_NAME = GIT_URL.split('/')[-1].split('.')[-2]
+CLONE_LOOKML_PATH = "lookml_project"
+PATH_TO_LOOKML_PROJECT = CLONE_LOOKML_PATH + "/" + REPO_NAME
+PATH_TO_LOOKML_PROJECT_VIEWS = PATH_TO_LOOKML_PROJECT + "/views"
+
+MANIFEST_FILE = "../target/manifest.json"
 COMPILATION_MESSAGE = "You may need to run dbt compile first."
 
 
-def get_manifest(path_to_project):
+class CloneProgress(RemoteProgress):
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        if message:
+            print(message)
+
+
+def get_lookml_files(GIT_URL, BRANCH, PATH_TO_LOOKML_PROJECT):
+    """
+    Download lookml files from github.
+    """
+    try:
+        if PATH_TO_LOOKML_PROJECT_VIEWS:
+            print('Pulling from %s' % GIT_URL)
+        else:
+            print('Cloning from %s' % GIT_URL)
+        git.Repo.clone_from(url=GIT_URL, to_path=CLONE_LOOKML_PATH,
+                            branch= BRANCH, progress=CloneProgress())
+    except:
+        pass
+
+def push_lookml_files(GIT_URL, BRANCH, PATH_TO_LOOKML_PROJECT):
+    """
+    Push lookml files from github. (Not finished yet.)
+    """
+    try:
+        print('Pushing to %s' % GIT_URL)
+        git.Repo.add(url=GIT_URL, to_path=CLONE_LOOKML_PATH,
+                            branch= BRANCH, progress=CloneProgress())
+
+        git.Repo.commit(url=GIT_URL, to_path=CLONE_LOOKML_PATH,
+                            branch= BRANCH, progress=CloneProgress())
+
+        git.Repo.push(url=GIT_URL, to_path=CLONE_LOOKML_PATH,
+                            branch= BRANCH, progress=CloneProgress())
+    except:
+        pass
+
+
+def get_manifest(MANIFEST_FILE):
     """
     Parse the manifest file as this is faster than importing the manifest object
     from dbt.
     """
-    manifest_path = os.path.join(path_to_project, MANIFEST_FILE)
+    manifest_path = os.path.join(MANIFEST_FILE)
     try:
         with open(manifest_path) as f:
             manifest = json.load(f)
@@ -34,8 +83,10 @@ def get_column_description(manifest, model_name, column_name):
     for fqn, node in manifest["nodes"].items():
         if node["name"] == model_name and node["resource_type"] == "model":
             matching_models.append(node)
-
-    model = matching_models[0]
+    try:
+        model = matching_models[0]
+    except:
+        return None
 
     try:
         column = model["columns"].get(column_name)
@@ -46,13 +97,17 @@ def get_column_description(manifest, model_name, column_name):
 
 
 # load the manifest.json file -- just do this once
-manifest = get_manifest(PATH_TO_DBT_PROJECT)
+manifest = get_manifest(MANIFEST_FILE)
+
+# Grab all lookml files
+get_lookml_files(GIT_URL, BRANCH, PATH_TO_LOOKML_PROJECT)
 
 # For each file in the lookml project
-for file_name in os.listdir(PATH_TO_LOOKML_PROJECT):
-    path_to_lookml_file = os.path.join(PATH_TO_LOOKML_PROJECT, file_name)
+for file_name in os.listdir(PATH_TO_LOOKML_PROJECT_VIEWS):
+    path_to_lookml_file = os.path.join(PATH_TO_LOOKML_PROJECT_VIEWS, file_name)
 
     # load the lookml
+    print('Loading Looker View : ', file_name)
     with open(path_to_lookml_file, "r") as file:
         lookml = lkml.load(file)
 
@@ -69,12 +124,48 @@ for file_name in os.listdir(PATH_TO_LOOKML_PROJECT):
             if dimension_description:
                 # update the description based on the project's description
                 dimension["description"] = dimension_description
+                print('Description found   : ', dimension_name + ':: ' + dimension_description)
 
     # dump the lmkl to a target directory
-    os.makedirs(PATH_TO_TARGET_LOOKML_PROJECT, exist_ok=True)
-    target_lookml_file = os.path.join(PATH_TO_TARGET_LOOKML_PROJECT, file_name)
+    os.makedirs(PATH_TO_LOOKML_PROJECT_VIEWS, exist_ok=True)
+    target_lookml_file = os.path.join(PATH_TO_LOOKML_PROJECT_VIEWS, file_name)
     with open(target_lookml_file, "w+") as file:
         lkml.dump(lookml, file)
+    print('Finished Looker View: ', file_name)
+    print('\n')
+
+
+# # Validate if new LookML files are valid
+# #push_lookml_files(GIT_URL, BRANCH, PATH_TO_LOOKML_PROJECT) # TBD (didn't have time)
+#
+#
+# ### Requires API v. 3.1 -- set this in config.yml
+#
+# ### ------- HERE ARE PARAMETERS TO CONFIGURE -------
+# host = 'HOSTNAME'
+#
+# ### ------- OPEN THE CONFIG FILE and INSTANTIATE API -------
+# f = open('config.yml')
+# params = yaml.load(f, Loader=yaml.FullLoader)
+# f.close()
+#
+# my_host = params['hosts'][host]['host']
+# my_project_id = params['hosts'][host]['project_id']
+# my_secret = params['hosts'][host]['secret']
+# my_token = params['hosts'][host]['token']
+#
+# looker = LookerApi(host=my_host,
+#                  token=my_token,
+#                  secret = my_secret)
+#
+# ### ------- GET AND PRINT THE LOOKML RESULTS -------
+# data = looker.validate_project(my_project_id)
+# print("LookML validation run results: ", data)
+#
+# ### ------- Done -------
+# print('Done. {} errors found.'.format(length(data['errors'])))
+
+
 
 
 # To-do:
